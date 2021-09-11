46caf3b93a0b9ea733e0e60244b9a80b80fa31a3
saz2nitk <saz2nitk@gmail.com>
2021-09-11 10:57:13 +0530

    created collectData fn

--- bin/dataIngestion.py
+++ bin/dataIngestion.py
@@ -5,6 +5,11 @@ Created on Sat Sep 11 09:29:58 2021
 @author: saz2n
 """
 import os
+import flask
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
 
 class Ingest:
     
@@ -38,4 +43,18 @@ class Ingest:
             contentList = self.readContentFromDir(basePath,className)
             finalData[className] = contentList
         return finalData
+
+@app.route('/fetch')
+def getData():
+    ingestObj = Ingest()
+    basePath = r'D:\Academia\Learning\GreyAtom\GADep1109\data\20news-18828'
+    finalData = ingestObj.collectData(basePath)
+    return json.dumps(finalData)
+
+app.run('localhost',5000)
+
+
+
+
+
         
