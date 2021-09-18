91c59ae4bc8239fd235b19eee00d869e29b79dea
saz2nitk <saz2nitk@gmail.com>
2021-09-18 11:02:23 +0530

    deployment module added

--- bin/preprocessor.py
+++ bin/preprocessor.py
@@ -5,3 +5,16 @@ Created on Sat Sep 11 09:30:33 2021
 @author: saz2n
 """
 
+class Preprocess:
+    
+    def __init__(self):
+        self.specialChars=[',','#','.',',','$','>','<','\\']
+    
+    
+    def removeSpecialChar(self,text):
+        for specialChar in self.specialChars:
+            text = text.replace(specialChar,'')
+        return text
+    
+    def stemWords(self):
+        pass
